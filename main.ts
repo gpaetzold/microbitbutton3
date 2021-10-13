a0006a7d64f0dfe52d0414cd3ddf5d01e8caee15
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-10-13 13:59:42 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        basic.showNumber(1)
+        basic.pause(2000)
+    } else {
+    	
+    }
 })
